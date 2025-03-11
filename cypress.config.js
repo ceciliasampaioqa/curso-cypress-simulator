67e1f667ef9b10bc0bdefb480bd8022566c19a5a
ceciliasampaioqa <ceciliasampaio.qa@gmail.com>
2025-03-11 09:37:15 -0300

    Configure cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,5 @@
 const { defineConfig } = require('cypress')
 
-module.exports = defineConfig({
-  viewportHeight: 1024,
-  viewportWidth: 1700,
-  e2e: {
-    fixturesFolder: false
-  }
-})
-//Com a configuração acima, foi substituida as dimensões de altura e largura padrão do Cypress,
-//além de não usarmos a pasta cypress/fixtures/
-const { defineConfig } = require('cypress')
-
 const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
